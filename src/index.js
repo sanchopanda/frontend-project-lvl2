ce5cff936d7ae37747a9b6fc9679408c46b29177
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-05-14 17:23:14 +0300

    add options and arguments

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ export default () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>');
 
   program.parse();
 };
