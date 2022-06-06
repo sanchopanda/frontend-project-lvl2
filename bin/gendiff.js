c99c885b32dd124c9a2579225fcd3fc090770fda
Alexander <ivanovaa1992@gmail.com>
2022-06-06 22:50:58 +0300

    update bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,11 +8,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
   });
 
 program.parse();
